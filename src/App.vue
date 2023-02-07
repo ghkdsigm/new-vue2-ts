604dbe9ee721221abd93eee94bf51f7c961cf7d6
황승현 <ghkdsigm3@gmail.com>
2023-02-07 19:02:51 +0900

    ts2 update

---
+++ src/App.vue
@@ -0,0 +1,133 @@
+<template>
+  <div>
+    <header>
+      <h1>Vue Todo With TypeScript</h1>
+    </header>
+    <main>
+      <TodoInput
+        :item="todoText"
+        @input="updateTodoText"
+        @add="addTodoItem"
+      ></TodoInput>
+      <!-- <input type="text" v-model="todoText"> -->
+      <div>
+        <ul>
+          <TodoListItem
+            v-for="(todoItem, index) in todoItems"
+            :key="index"
+            :index="index"
+            :todoItem="todoItem"
+            @remove="removeTodoItem"
+            @toggle="toggleItem"
+          ></TodoListItem>
+        </ul>
+      </div>
+      <div>
+        <button class="btn" @click="removeAll" type="button">전체 삭제</button>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoListItem from "./components/TodoListItem.vue";
+
+const STORAGE_KEY = "vue-todo-ts-v1";
+const storage = {
+  // localStorage 에 저장할 데이터
+  save(todoItems: Todo[]) {
+    const parsed = JSON.stringify(todoItems);
+    localStorage.setItem(STORAGE_KEY, parsed);
+  },
+
+  // 조회 api
+  fetch(): Todo[] {
+    const todoItems = localStorage.getItem(STORAGE_KEY) || "[]";
+    // 배열 문자열을 Object 형으로 변형
+    const result = JSON.parse(todoItems);
+    return result;
+  },
+};
+
+// 할일 객체의 타입
+export interface Todo {
+  title: string;
+  done: boolean;
+}
+
+export default Vue.extend({
+  components: { TodoInput, TodoListItem },
+  data() {
+    return {
+      todoText: "" as string,
+      todoItems: [] as Todo[],
+    };
+  },
+  methods: {
+    removeAll() {
+      this.todoItems = [];
+      storage.save(this.todoItems);
+    },
+    updateTodoText(value: string) {
+      this.todoText = value;
+    },
+    addTodoItem() {
+      const value = this.todoText;
+      const todo: Todo = {
+        title: value,
+        done: false,
+      };
+      this.todoItems.push(todo);
+      storage.save(this.todoItems);
+      // localStorage.setItem(value, value);
+      this.initTodoText();
+    },
+    initTodoText() {
+      this.todoText = "";
+    },
+
+    // 할일 조회
+    fetchTodoItems() {
+      this.todoItems = storage.fetch().sort((a, b) => {
+        if (a.title < b.title) {
+          return -1;
+        }
+        if (a.title < b.title) {
+          return 1;
+        }
+        return 0;
+      });
+    },
+
+    // 할 일 삭제
+    removeTodoItem(index: number) {
+      console.log("remove", index);
+      this.todoItems.splice(index, 1);
+      storage.save(this.todoItems);
+    },
+
+    // 할 일 상태 변경
+    toggleItem(todoItem: Todo, index: number) {
+      this.todoItems.splice(index, 1, {
+        ...todoItem,
+        done: !todoItem.done,
+      });
+      storage.save(this.todoItems);
+    },
+  },
+  created() {
+    this.fetchTodoItems();
+  },
+});
+</script>
+
+<style scoped>
+.btn {
+  background-color: beige;
+  border: none;
+  border-radius: 10%;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,59 @@
+<template>
+  <li class="wrapper">
+    <span class="item" :class="todoItemClass" @click="toggleItem">{{
+      todoItem.title
+    }}</span>
+    <button class="btn" @click="removeItem">삭제</button>
+  </li>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from "vue";
+import { Todo } from "../App.vue";
+
+export default Vue.extend({
+  props: {
+    todoItem: Object as PropType<Todo>,
+    index: Number,
+  },
+  computed: {
+    // style 조건
+    todoItemClass(): string | null {
+      return this.todoItem.done ? "complete" : null;
+    },
+  },
+  methods: {
+    toggleItem() {
+      this.$emit("toggle", this.todoItem, this.index);
+    },
+    removeItem() {
+      this.$emit("remove", this.index);
+    },
+  },
+});
+</script>
+
+<style scoped>
+.wrapper {
+  display: grid;
+  grid-template-columns: 6fr 1fr 5fr;
+  margin-bottom: 10px;
+}
+.item {
+  cursor: pointer;
+  border: 2px solid;
+  border-color: bisque;
+  border-radius: 10%;
+  margin-right: 10px;
+}
+.complete {
+  text-decoration: line-through;
+}
+.btn {
+  float: right;
+  background-color: beige;
+  border: none;
+  border-radius: 10%;
+  cursor: pointer;
+}
+</style>
